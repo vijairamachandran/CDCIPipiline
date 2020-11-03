0be7f02fb6f37ea8ae0a5e91d5186ed251eb8af9
vijai <vramachandran@medline.com>
2020-11-02 23:22:55 -0500

    Add project files.

---
+++ CDCIPipilineDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CDCIPipilineDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
